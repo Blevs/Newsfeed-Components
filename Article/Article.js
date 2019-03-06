edb3f32a34a481db8c3fa111b420bf74f5400994
Henry Blevins <heblevi@gmail.com>
2019-03-06 14:06:19 -0500

    Hacked together a collapsing/expanding animation in css
    So apparently this is not flawlessly possible in pure css because transitions
    are not supported on auto/100% value changes. We would either need to used fixed
    pixel height changes, unaware of content size, or a variety of hacks. I decided
    to animate the content collapsing and expanding, and have the container size
    jump to the correct height at the end of the animation, because you get a
    gestalt effect of the container moving, even though it is still slightly
    jarring.
    
    Of course, the desired effect is entirely possible using JavaScript, but this
    seemed like a fun challenge.

--- Article/Article.js
+++ Article/Article.js
@@ -2,23 +2,22 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector(".expandButton");
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.content = domElement.querySelector(".content");
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    if (this.domElement.classList.contains("article-open")) {
-      this.domElement.classList.remove("article-open");
+    if (this.content.classList.contains("article-open")) {
+      this.content.classList.remove("article-open");
+      this.content.classList.add("article-close");
       this.expandButton.textContent = "expand";
     } else {
-      this.domElement.classList.add("article-open");
+      this.content.classList.add("article-open");
+      this.content.classList.remove("article-close");
       this.expandButton.textContent = "collapse";
     }
   }
